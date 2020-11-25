7c777f8f1332b5a9e8bcebe8c623ab36f2b5f63b
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-25 05:46:40 -0800

    updated footer look

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,50 +1,43 @@
 import React from "react";
 import "./styles.scss";
-import { graphql, useStaticQuery, Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image/withIEPolyfill";
-
+import { AiOutlineInstagram } from "react-icons/ai";
 const Footer = () => {
   const data = useStaticQuery(graphql`
     query {
       file(relativePath: { eq: "charis.jpg" }) {
         childImageSharp {
-          fixed(height: 75, width: 75, quality: 60) {
+          fixed(height: 100, width: 100, quality: 90) {
             ...GatsbyImageSharpFixed
           }
         }
       }
     }
   `);
-
+  const d = new Date();
+  const currentYear = d.getFullYear();
   return (
-    <footer className="footer">
+    <footer className="custom-footer">
+      <div className="footer-copyright">
+        <p>All Site Content is property of Charis Cheung {currentYear}.</p>
+      </div>
       <div className="footer-image-text">
         <Img fixed={data.file.childImageSharp.fixed} objectPosition="bottom" />
         {/* <p>{data.allDatoCmsHomePage.edges[0].node.footerQuote}</p> */}
       </div>
       <div className="footer-links">
+        <a
+          href="https://charischeung.com/"
+          style={{ display: "flex", alignItems: "center", gap: "3vw" }}
+        >
+          charischeung.com
+        </a>
         <ul>
           <li>
-            <Link to="/presets">Preset</Link>
-          </li>
-          <li>
-            <Link to="/presets">Help</Link>
-          </li>
-          <li>
-            <Link to="/presets">Contact</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a
-              href="https://charischeung.com/"
-              style={{ display: "flex", alignItems: "center", gap: "3vw" }}
-            >
-              My Portfolio
+            <a href="https://www.instagram.com/charis.cheung/">
+              <AiOutlineInstagram />
             </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/charis.cheung/">Instagram</a>
           </li>
         </ul>
       </div>
