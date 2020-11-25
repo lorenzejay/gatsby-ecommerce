5cc225b48756b21124fcb18f416384f5dddf1778
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-25 06:38:57 -0800

    new look for presets

--- src/templates/Preset/index.js
+++ src/templates/Preset/index.js
@@ -21,7 +21,7 @@ const ProductTemplate = ({ pageContext }) => {
   `);
   const { preset } = pageContext;
   const desktopPresetDetails = data.allDatoCmsPresetDetail.nodes[0].desktopDetails;
-  console.log(desktopPresetDetails);
+  console.log(preset.beforeAndAfter);
   const beforeAfter1 = preset.beforeAndAfter[0].url;
   const beforeAfter2 = preset.beforeAndAfter[1].url;
   const beforeAfter3 = preset.beforeAndAfter2.length !== 0 && preset.beforeAndAfter2[0].url;
@@ -29,42 +29,60 @@ const ProductTemplate = ({ pageContext }) => {
   return (
     <Layout>
       <div className="preset-container">
-        <Link to="/presets">Back</Link>
-        <h1>{preset.name}</h1>
-        <div className="preset-split">
-          <div className="preset-display-container">
-            <Carousel>
-              <Carousel.Item>
-                <ReactCompareImage
-                  className="preset-compare"
-                  leftImage={beforeAfter1}
-                  rightImage={beforeAfter2}
-                  leftImageCss={{
-                    objectFit: "contain",
-                    objectPosition: "center",
-                    width: "inherit",
-                  }}
-                  rightImageCss={{
-                    objectFit: "contain",
-                    objectPosition: "center",
-                    width: "inherit",
-                  }}
-                />
-              </Carousel.Item>
-              {beforeAfter3 && beforeAfter4 && (
-                <Carousel.Item>
-                  <ReactCompareImage
-                    className="preset-compare"
-                    leftImage={beforeAfter3}
-                    rightImage={beforeAfter4}
-                  />
-                </Carousel.Item>
-              )}
-            </Carousel>
-            <p className="preset-description">{preset.description}</p>
+        <div className="preset-container-title">
+          <h1>
+            {preset.name} - <span>${preset.price}</span>
+          </h1>
+          <div>
+            <p>{preset.description}</p>
           </div>
+          <button
+            className="snipcart-add-item"
+            data-item-id={preset.id}
+            data-item-price={preset.price}
+            data-item-url={`https://charispresets.com/presets/${preset.slug}`}
+            data-item-description={preset.description}
+            data-item-name={preset.name}
+            data-item-image={preset.image.url}
+            data-item-file-guid={preset.fileGuid}
+          >
+            <p>Add to cart</p>
+          </button>
+        </div>
 
-          <div className="preset-purchase-container">
+        {/* <div className="preset-split">
+          <div className="preset-display-container"> */}
+        <Carousel>
+          <Carousel.Item>
+            <ReactCompareImage
+              className="preset-compare"
+              leftImage={beforeAfter1}
+              rightImage={beforeAfter2}
+              leftImageCss={{
+                objectFit: "contain",
+                objectPosition: "center",
+              }}
+              rightImageCss={{
+                objectFit: "contain",
+                objectPosition: "center",
+              }}
+            />
+          </Carousel.Item>
+          {beforeAfter3 && beforeAfter4 && (
+            <Carousel.Item>
+              <ReactCompareImage
+                className="preset-compare"
+                leftImage={beforeAfter3}
+                rightImage={beforeAfter4}
+              />
+            </Carousel.Item>
+          )}
+        </Carousel>
+
+        {/* <p className="preset-description">{preset.description}</p> */}
+        {/* </div> */}
+
+        {/* <div className="preset-purchase-container">
             <h2>${preset.price}</h2>
 
             <button
@@ -79,8 +97,9 @@ const ProductTemplate = ({ pageContext }) => {
             >
               <p>Add to cart</p>
             </button>
-          </div>
-        </div>
+          </div> */}
+        {/* </div> */}
+        <Link to="/presets">Back to all presets</Link>
         <div className="preset-disclaimer-wrapper">
           {desktopPresetDetails.map((detail, i) => (
             <div className="preset-disclaimer-content" key={i}>
