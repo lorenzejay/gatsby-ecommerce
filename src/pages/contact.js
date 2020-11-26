68dd850e996404a6ab90c2e0905d0fe23f216a2a
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-26 11:31:21 -0800

    created a contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Layout from "../layouts/index";
+import "../style/Contact.scss";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <div className="Contact">
+        <h1>Contact</h1>
+        <p>If you need additional help installing your presets, please contact using this form</p>
+        <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="form-details">
+            <input name="name" type="text" placeholder="Name" />
+            <input name="email" type="email" placeholder="Email" />
+
+            <textarea name="message" placeholder="Message" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
