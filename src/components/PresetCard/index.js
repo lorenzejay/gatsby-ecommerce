3529a0ef45196ff6fcf7807ee9a73bfa363c4a52
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-26 10:12:32 -0800

    preset card updated

--- src/components/PresetCard/index.js
+++ src/components/PresetCard/index.js
@@ -8,25 +8,31 @@ const PresetCard = ({ image, title, price, link, id, description, guid, isDeskto
       <Link to={link}>
         <img src={image} alt={title} />
       </Link>
+
       <div className="preset-card-container">
-        <h3>{title}</h3>
-      </div>
+        <div className="preset-card-text">
+          <h3>{title}</h3>
 
-      <div className="preset-card-price-addcart">
-        <p>${parseInt(price)}</p>
-
-        <button
-          className="snipcart-add-item"
-          data-item-id={id}
-          data-item-price={price}
-          data-item-url={`https://charispresets.com/presets/${slug}`}
-          data-item-description={description}
-          data-item-name={title}
-          data-item-image={image}
-          data-item-file-guid={guid}
-        >
-          <p>Add to cart</p>
-        </button>
+          <div className="preset-card-price-addcart">
+            <p>${parseInt(price)}</p>
+            <p className="preset-card-description ">{description}</p>
+            <button
+              className="snipcart-add-item"
+              data-item-id={id}
+              data-item-price={price}
+              data-item-url={`https://charispresets.com/presets/${slug}`}
+              data-item-description={description}
+              data-item-name={title}
+              data-item-image={image}
+              data-item-file-guid={guid}
+            >
+              <p>Add to cart</p>
+            </button>
+            <button>
+              <p>View</p>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
