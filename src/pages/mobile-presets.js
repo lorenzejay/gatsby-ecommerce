eac71b9be38c6fd5c1357c2404ff5b7677165cf0
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-27 06:41:04 -0800

    added slug into graphql query for mobile

--- src/pages/mobile-presets.js
+++ src/pages/mobile-presets.js
@@ -18,6 +18,7 @@ export default function MobilePresets({ className }) {
             fileGuid
             description
             price
+            slug
             image {
               url
             }
